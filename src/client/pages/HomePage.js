7bcc287865d0006c354c17ddaced38dc32e892d9
Pooja Chavan <poochava1@publicisgroupe.net>
2022-09-27 14:55:28 +0530

    webhook test 1

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div className='center-align' style={{marginTop: '200px'}}>
-            <h3>Welcome to SSR in React project!</h3>
+            <h3>Welcome to SSR!</h3>
             <p>This is a simple server side rendering demo!</p>
         </div>
         )
