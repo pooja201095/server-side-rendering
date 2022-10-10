701f603ee017a71507ecbbc1a14ffe778c164f52
pooja201095 <poojachavan153@gmail.com>
2022-10-10 09:55:26 +0530

    Update HomePage.js

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div className='center-align' style={{marginTop: '200px'}}>
-            <h3>Welcome to SSR!</h3>
+            <h3>Welcome to SSR 2.0!</h3>
             <p>This is a simple server side rendering demo!</p>
         </div>
         )
